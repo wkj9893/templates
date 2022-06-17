0390171d6d36f56d0f3d49c55e878094754b8c7b
wkj <wkj9893@gmail.com>
2022-06-17 10:33:02 +0800

    update

--- default/flake.nix
+++ default/flake.nix
@@ -5,9 +5,9 @@
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, utils, }:
-    utils.lib.eachDefaultSystem (system: rec {
-      pkgs = import nixpkgs { inherit system; };
-      devShells.default = pkgs.mkShell { buildInputs = with pkgs; [ ]; };
-    });
+  outputs = { self, nixpkgs, utils, }: utils.lib.eachDefaultSystem (system: rec {
+    pkgs = import nixpkgs { inherit system; };
+    packages.default = pkgs.hello;
+    devShells.default = pkgs.mkShell { buildInputs = with pkgs; [ ]; };
+  });
 }
